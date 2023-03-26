da363f66c37fcc17d6557f501b8c9677089d68f3
John Wordell <wordell.john@protonmail.com>
2023-03-25 23:40:42 -0400

    move db info to properties file, so repo can be public without leaking the info. once we have RDS setup I will put it on the drive.

--- src/main/kotlin/com/urepair/dao/DatabaseFactory.kt
+++ src/main/kotlin/com/urepair/dao/DatabaseFactory.kt
@@ -5,18 +5,30 @@ import kotlinx.coroutines.*
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.*
 import org.jetbrains.exposed.sql.transactions.experimental.*
+// import java.io.FileInputStream
+// import java.util.*
 
 object DatabaseFactory {
-    fun init() {
-        // val rdsEndpoint = "your-rds-endpoint"
-        // val rdsPort = "your-rds-port"
-        // val dbName = "urepair-db"
-        // val username = "your-rds-username"
-        // val password = "your-rds-password"
+//    private fun loadProperties(): Properties {
+//        val properties = Properties()
+//        val propertiesFile = FileInputStream("dbconfig.properties")
+//        properties.load(propertiesFile)
+//        propertiesFile.close()
+//        return properties
+//    }
 
-        // val driverClassName = "org.postgresql.Driver"
+    fun init() {
+        // uncomment this section and edit dbconfig.properties to switch from local to amazon rds
+        // val properties = loadProperties()
+        // val rdsEndpoint = properties.getProperty("rdsEndpoint")
+        // val rdsPort = properties.getProperty("rdsPort")
+        // val dbName = properties.getProperty("dbName")
+        // val username = properties.getProperty("username")
+        // val password = properties.getProperty("password")
+        // val driverClassName = properties.getProperty("driverClassName")
         // val jdbcUrl = "jdbc:postgresql://$rdsEndpoint:$rdsPort/$dbName"
         // val database = Database.connect(jdbcUrl, driverClassName, user = username, password = password)
+
         val driverClassName = "org.h2.Driver"
         val jdbcURL = "jdbc:h2:file:./build/db"
         val database = Database.connect(jdbcURL, driverClassName)
@@ -26,6 +38,7 @@ object DatabaseFactory {
             SchemaUtils.create(UserTable)
         }
     }
+
     suspend fun <T> dbQuery(block: suspend () -> T): T =
         newSuspendedTransaction(Dispatchers.IO) { block() }
 }
