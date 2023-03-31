5445641d788b47f97dea1e9763690e2cb3906869
John Wordell <wordell.john@protonmail.com>
2023-03-31 00:56:43 -0400

    Another attempt at auto applying settings...

--- .platform/00_copy_nginx_conf.sh
+++ .platform/00_copy_nginx_conf.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+echo "Copying Nginx configuration file" >> /var/log/eb-custom-platform.log
 cp .platform/nginx/https.conf /etc/nginx/conf.d/
+echo "Finished copying Nginx configuration file" >> /var/log/eb-custom-platform.log
