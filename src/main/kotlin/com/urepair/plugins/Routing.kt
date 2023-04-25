6b69cf833cb83850f17ecfb4c52c18b8b02601b9
John Wordell <wordell.john@protonmail.com>
2023-04-25 13:48:49 -0400

    checkout a specific tag from front end

--- src/main/kotlin/com/urepair/plugins/Routing.kt
+++ src/main/kotlin/com/urepair/plugins/Routing.kt
@@ -23,7 +23,6 @@ import com.urepair.routes.removeUserRoute
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
-import io.ktor.server.http.content.static
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
