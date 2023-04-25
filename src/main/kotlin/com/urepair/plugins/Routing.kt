bc2b39bff319a7cfb26681e467bad0883a4dff88
John Wordell <wordell.john@protonmail.com>
2023-04-25 13:48:39 -0400

    checkout a specific tag from front end

--- src/main/kotlin/com/urepair/plugins/Routing.kt
+++ src/main/kotlin/com/urepair/plugins/Routing.kt
@@ -20,16 +20,26 @@ import com.urepair.routes.removeEquipmentRoute
 import com.urepair.routes.removeIssueCountRoute
 import com.urepair.routes.removeIssueRoute
 import com.urepair.routes.removeUserRoute
-import io.ktor.server.application.Application
-import io.ktor.server.http.content.react
-import io.ktor.server.http.content.singlePageApplication
-import io.ktor.server.routing.routing
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.http.content.static
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
         singlePageApplication {
             useResources = true
             react("react-app")
+        }
+        get("/.well-known/pki-validation/56E8D287CD70A53033EF467DD258ED8C.txt") {
+            val text = javaClass.classLoader.getResourceAsStream("56E8D287CD70A53033EF467DD258ED8C.txt")?.bufferedReader().use { it?.readText() }
+            if (text != null) {
+                call.respondText(text, ContentType.Text.Plain)
+            } else {
+                call.respond(HttpStatusCode.NotFound, "txt file not found in resources")
+            }
         }
         listEquipmentRoute()
         getEquipmentRoute()
