59e43ea12265ed46ef192bd25cbd3bf95e58f0b6
John Wordell <wordell.john@protonmail.com>
2023-05-05 15:32:54 -0400

    Try HSTS

--- src/main/kotlin/com/urepair/utilities/Sanitize.kt
+++ src/main/kotlin/com/urepair/utilities/Sanitize.kt
@@ -3,7 +3,7 @@ package com.urepair.utilities
 import kotlinx.html.span
 import kotlinx.html.stream.createHTML
 fun sanitize(input: String): String {
-    val sanitizedHtml =  createHTML().span {
+    val sanitizedHtml = createHTML().span {
         text(input)
     }.toString()
     val withoutHtmlTags = sanitizedHtml.replace(Regex("<[^>]*>"), "")
