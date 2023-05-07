5787aba5cf1d2ec561bffe9539b7cdf981d69979
John Wordell <wordell.john@protonmail.com>
2023-05-07 01:36:43 -0400

    Fixed it, I think for real this time :) Once again into the breach Once more, Once more into the breach

--- src/main/kotlin/com/urepair/Application.kt
+++ src/main/kotlin/com/urepair/Application.kt
@@ -1,7 +1,6 @@
 package com.urepair
 
 import at.favre.lib.crypto.bcrypt.BCrypt
-import com.typesafe.config.ConfigFactory
 import com.urepair.dao.DatabaseFactory
 import com.urepair.plugins.configureRouting
 import com.urepair.plugins.configureSerialization
@@ -9,16 +8,18 @@ import com.urepair.utilities.getSecret
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
+import io.ktor.network.tls.certificates.buildKeyStore
+import io.ktor.network.tls.certificates.saveToFile
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.auth.Authentication
 import io.ktor.server.auth.UserIdPrincipal
 import io.ktor.server.auth.basic
 import io.ktor.server.auth.session
-import io.ktor.server.config.HoconApplicationConfig
 import io.ktor.server.engine.applicationEngineEnvironment
-import io.ktor.server.engine.commandLineEnvironment
+import io.ktor.server.engine.connector
 import io.ktor.server.engine.embeddedServer
+import io.ktor.server.engine.sslConnector
 import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.cors.routing.CORS
 import io.ktor.server.plugins.hsts.HSTS
@@ -31,43 +32,42 @@ import io.ktor.server.sessions.Sessions
 import io.ktor.server.sessions.cookie
 import io.ktor.util.hex
 import kotlinx.serialization.json.jsonPrimitive
+import org.slf4j.LoggerFactory
+import java.io.File
 import kotlin.time.Duration.Companion.seconds
 
 data class StaffSession(val userID: String)
 
-fun configureSSLFromSecrets(secretPath: String): HoconApplicationConfig {
-    val awsSecret = getSecret(secretPath)
+fun main() {
+    val awsSecret = getSecret("urepair/jks")
     val keyStorePassword = awsSecret["KEY_STORE_PASSWORD"]?.jsonPrimitive.toString()
     val keyStoreAlias = awsSecret["KEY_STORE_ALIAS"]?.jsonPrimitive.toString()
-
-    val sslConfig = ConfigFactory.parseString(
-        """
-        ktor {
-            deployment {
-                sslConnector {
-                    keyStore = urepair_me.jks
-                    keyAlias = $keyStoreAlias
-                    keyStorePassword = $keyStorePassword
-                    privateKeyPassword = $keyStorePassword
-                }
-            }
+    val keyStoreFile = File("urepair_me.jks")
+    val keyStore = buildKeyStore {
+        certificate(keyStoreAlias) {
+            password = keyStorePassword
         }
-        """.trimIndent(),
-    )
+    }
+    keyStore.saveToFile(keyStoreFile, keyStorePassword)
 
-    val newConfig = ConfigFactory.load().withFallback(sslConfig)
-    return HoconApplicationConfig(newConfig)
-}
-
-fun main(args: Array<String>) {
-    val customConfig = configureSSLFromSecrets("urepair/jks")
-    val customEnvironment = applicationEngineEnvironment {
-        this.config = customConfig
-        this.module(Application::module)
-        this.log = commandLineEnvironment(args).log
+    val environment = applicationEngineEnvironment {
+        log = LoggerFactory.getLogger("ktor.application")
+        connector {
+            port = System.getenv("PORT")?.toInt() ?: 5000
+        }
+        sslConnector(
+            keyStore = keyStore,
+            keyAlias = System.getenv("KEY_STORE_ALIAS"),
+            keyStorePassword = { keyStorePassword.toCharArray() },
+            privateKeyPassword = { keyStorePassword.toCharArray() },
+        ) {
+            port = 8433
+            keyStorePath = keyStoreFile
+        }
+        module(Application::module) // Update this line to use the appropriate module
     }
 
-    embeddedServer(Netty, environment = customEnvironment).start(wait = true)
+    embeddedServer(Netty, environment).start(wait = true)
 }
 
 fun Application.module() {
